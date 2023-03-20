65ce4cff64a1cef88f8baa0cfbf6dae5d2b1be7d
Changhyeon Yoon <hyeon120411@gmail.com>
2023-03-20 14:57:33 +0900

    Feat : connect wallet

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import {StrictMode} from "react";
+import {Web3ReactProvider} from "@web3-react/core";
+import {Web3Provider} from "@ethersproject/providers";
+import {ExternalProvider} from "@ethersproject/providers/src.ts/web3-provider";
+
+function getLibrary(provider: ExternalProvider) {
+  return new Web3Provider(provider, "any");
+}
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <StrictMode>
+        <Web3ReactProvider getLibrary={getLibrary}>
+          <Component {...pageProps} />
+        </Web3ReactProvider>
+      </StrictMode>
+  )
 }
